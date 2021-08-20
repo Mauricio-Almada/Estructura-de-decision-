7cb5fd057a567c71f65840bafb9c100193acd1a1
Mauricio-Almada <81022458+Mauricio-Almada@users.noreply.github.com>
2021-08-20 17:06:13 -0300

    Add files via upload

---
+++ Ejercicio 07/Ejercicio 07.cpp
@@ -0,0 +1,28 @@
+/*Hacer un programa para ingresar por teclado tres números e informar con una
+leyenda aclaratoria si los tres son todos distintos entre sí, caso contrario no
+emitir nada. Ayuda: Si A es distinto de B y B es distinto de C, eso no signica que
+A y C sean distintos. Ejemplo: A=8, B=6 y C=8.*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numero1, numero2, numero3;
+
+    cout << "Igresar tres numeros: ";
+    cin >> numero1;
+    cin >> numero2;
+    cin >> numero3;
+
+    if (numero1 != numero2 && numero1 != numero3 && numero2 != numero1 && numero2 != numero3 && numero3 != numero1 != numero3 != numero2){
+        cout << "Los tres numeros son distintos";
+    }
+
+
+
+
+
+
+
+    return 0;
+}
