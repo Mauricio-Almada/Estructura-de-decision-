9ca826dfb93817970a29e28bd5b38db811044cd4
Mauricio-Almada <81022458+Mauricio-Almada@users.noreply.github.com>
2021-08-26 22:58:19 -0300

    Add files via upload

---
+++ Ejercicio 17/Ejercicio 17.cpp
@@ -0,0 +1,45 @@
+/*Una empresa de electricidad cobra el servicio a sus clientes de acuerdo a la
+siguiente escala:
+$ 10 por kilovatio (kW) por el consumo hasta los primeros 100 kW de consumo.
+$ 12 por kW por el consumo excedente de 101 a 200 kW.
+$ 15 por kW por el consumo excedente de 201 kW en adelante.
+Hacer un programa para que, dado el consumo en kilovatios de un determinado
+cliente, el programa calcule e informe el total a pagar por el mismo.
+Ejemplo 1: Un consumo de 55 kW, se calculará: $ 10 x 55= $ 550
+Ejemplo 2: Un consumo de 125 kW, se calculará: $ 10 x 100 + $ 12 x 25 = $1300.
+Ejemplo 3: Un consumo de 250 kW, se calculará: $ 10 x 100 + $ 12 x 100 + $ 15
+x 50 = $ 2950.*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    const int KW100 = 10, KW200 = 12, KWEXTRA = 15;
+    int consumo, totalApagar;
+
+    cout << "Ingrese consumo: ";
+    cin >> consumo;
+
+    if (consumo <= 100){
+        totalApagar = consumo * KW100;
+    }
+    else{
+        if (consumo >= 101 && consumo <= 200){
+            totalApagar = (consumo - 100) * 12 + (KW100 * 100);
+        }
+        else{
+            if (consumo >= 201){
+                totalApagar = (consumo - 200) * 15 + (KW100 * 100) + (KW200 * 100);
+            }
+        }
+    }
+    cout << "Total a pagar: " << "$" << totalApagar;
+
+
+
+
+
+
+    return 0;
+}
